d9b179a389db9d0fdf3900d45c38b3cba323d137
Pavithra Kulathilaka <pavithrask@gmail.com>
2022-12-03 14:51:39 +0530

    logic for updating points of all the users. needs cleanup.

--- lib/game/worker.ex
+++ lib/game/worker.ex
@@ -1,6 +1,11 @@
 defmodule Game.Worker do
   use GenServer
 
+  import Ecto.Query
+
+  alias Game.Repo
+  alias Game.Score.User
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{})
   end
@@ -13,9 +18,11 @@ defmodule Game.Worker do
     # Do the desired work here
     # ...
     # Reschedule once more
+    IO.inspect state, label: "Gen server is doing some work........"
     now = DateTime.utc_now()
     last_query_time = nil
     state = %{max_number: :rand.uniform(100), last_query_time: last_query_time, timestamp: now}
+    update_work()
     IO.inspect state, label: "Gen server is done working!"
     schedule_work()
     {:noreply, state}
@@ -25,5 +32,38 @@ defmodule Game.Worker do
     Process.send_after(self(), :work, 10 * 1000)
   end
 
+  def update_work do
+    user = %{points: {:placeholder, :points}, updated_at: {:placeholder, :now}}
+
+    user_ids = Enum.to_list 1..100
+
+    now =
+      NaiveDateTime.utc_now()
+      |> NaiveDateTime.truncate(:second)
+
+    users =
+      Enum.map(user_ids, &%{
+        id: &1,
+        points: {:placeholder, :points},
+        updated_at: {:placeholder, :now}
+      })
+
+    placeholders = %{
+      points: :rand.uniform(100),
+      now: now
+    }
+
+    # Repo.all(from u in User, update: User, set: [points: fragment("floor(random()*100)")]
+    # |> Repo.update_all([]))
+
+    update(User, set: [points: fragment("floor(random()*100)")])
+    |> Repo.update_all([])
+
+    # Repo.update_all(
+    #   User,
+    #   users,
+    #   placeholders: placeholders
+    # )
+  end
 
 end
