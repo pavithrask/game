33d2d619dcc6103d1d628c4fdaeeb7d09d0e6aeb
Pavithra Kulathilaka <pavithrask@gmail.com>
2022-12-04 17:02:35 +0530

    refactoring the code

--- lib/game/worker.ex
+++ lib/game/worker.ex
@@ -8,38 +8,38 @@ defmodule Game.Worker do
   alias Game.Score
 
   def init(state) do
-    # Schedule work to be performed on start
     state = %{max_number: :rand.uniform(100), last_query_time: nil}
+    update_points()
     schedule_work()
     IO.inspect state, label: "state"
     {:ok, state}
   end
 
   def handle_call(:winners, _from, state) do
-    IO.inspect state, label: "state"
-    last_query_time = state[:last_query_time]
-    state = Map.put(state, :last_query_time, DateTime.utc_now())
-    IO.inspect state, label: "state"
+    IO.inspect state, label: "current_state"
+
+    previous_query_time = state[:last_query_time]
+    state = update_last_query_time(state, DateTime.utc_now())
+
+    IO.inspect state, label: "new_state"
 
     winners = Score.get_two_winners(state[:max_number])
+
     IO.inspect winners, label: "winners"
 
-    {:reply, %{winners: winners, timestamp: last_query_time}, state}
+    {:reply, %{winners: winners, timestamp: previous_query_time}, state}
   end
 
   def handle_info(:work, state) do
-    # Do the desired work here
-    IO.inspect DateTime.utc_now(), label: "WORKER START"
+    IO.inspect state, label: "current_state"
+    IO.inspect DateTime.utc_now(), label: "work in progress"
 
-    # last_query_time = nil
-    # state = %{max_number: :rand.uniform(100), last_query_time: last_query_time}
-    state = Map.put(state, :max_number, :rand.uniform(100))
+    state = update_state(state)
     update_points()
 
-    IO.inspect state, label: "STATE"
-    IO.inspect DateTime.utc_now(), label: "WORKER STOP"
+    IO.inspect DateTime.utc_now(), label: "work done"
+    IO.inspect state, label: "new_state"
 
-    # Reschedule once more
     schedule_work()
     {:noreply, state}
   end
@@ -69,9 +69,17 @@ defmodule Game.Worker do
     # Process.send_after(self(), :work, milliseconds_till_next)
   end
 
+  def update_state(state) do
+    Map.put(state, :max_number, :rand.uniform(100))
+  end
+
   def update_points do
     update(User, set: [points: fragment("floor(random()*100)")])
     |> Repo.update_all([])
+  end
+
+  def update_last_query_time(state, time) do
+    Map.put(state, :last_query_time, time)
   end
 
 end
