cda69c77dda15b4b42010eed9fe576f45aa7f160
Pavithra Kulathilaka <pavithrask@gmail.com>
2022-12-06 16:23:48 +0530

    optimizing the update_points function

--- lib/game/worker.ex
+++ lib/game/worker.ex
@@ -43,16 +43,46 @@ defmodule Game.Worker do
     {:noreply, state}
   end
 
-  def update_state(state) do
+  defp update_state(state) do
     Map.put(state, :max_number, :rand.uniform(100))
   end
 
-  def update_points do
-    update(User, set: [points: fragment("floor(random()*100)")])
+  defp update_id_range(%{start: start_id, end: end_id}) do
+    from(u in User, where: u.id > ^start_id and u.id <= ^end_id, update: [set: [points: fragment("floor(random()*100)")]])
     |> Repo.update_all([])
   end
 
-  def update_last_query_time(state, time) do
+  def update_id_range(start_id, start_id) do
+    IO.inspect start_id, label: "start_id"
+  end
+
+  def func(i) do
+    IO.puts("Function running")
+  end
+
+  defp update_points do
+    time1 = NaiveDateTime.utc_now
+
+    step = 10_000
+    ranges = Enum.to_list(0..990_000//step)
+    |> Enum.map(fn start_id -> %{start_id: start_id, end_id: start_id + step} end)
+
+    Repo.transaction(fn ->
+      Task.async_stream(ranges,
+        fn %{start_id: start_id, end_id: end_id} ->
+          from(u in User, where: u.id > ^start_id and u.id <= ^end_id,
+          update: [set: [points: fragment("floor(random()*100)"), updated_at: fragment("NOW()")]])
+        |> Repo.update_all([]) end, ordered: false, max_concurrency: 5)
+      |> Enum.reduce(0, fn {_, _}, _ -> IO.inspect(label: "fasdf") end)
+    end)
+
+
+    time2 = NaiveDateTime.utc_now
+    time_total = NaiveDateTime.diff(time2, time1, :microsecond)
+    IO.inspect time_total, label: "time"
+  end
+
+  defp update_last_query_time(state, time) do
     Map.put(state, :last_query_time, time)
   end
 
